b8ecb78d5e1e406bf10fe33ec1ba7624249b0c88
Theo SIGNORE <tsignore@e2r3p16.42.fr>
2020-07-02 21:33:19 +0200

    ex06 in progress

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
-void ft_print_comb(void);
-
+//void	ft_print_comb2(void);
+#include <unistd.h>
 int main()
 {
-	ft_print_comb();
+//	ft_print_comb2();
+	char c = '1';
+	c = '1'+ 1;
+	write(1, &c, 1);
 }
