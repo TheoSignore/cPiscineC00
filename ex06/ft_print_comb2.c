790ef0b574affcfd69d69f66c70f3f31d741276a
Theo SIGNORE <tsignore@e2r2p8.42.fr>
2020-07-03 14:36:00 +0200

    ex06 done

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -5,42 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/02 20:30:37 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/02 21:32:37 by tsignore         ###   ########.fr       */
+/*   Created: 2020/07/03 13:16:35 by tsignore          #+#    #+#             */
+/*   Updated: 2020/07/03 14:34:59 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	display(char d1, char d2, char d3, char d4)
-{
-	write(1, &d1, 1);
-	write(1, &d2, 1);
-	write(1, &d3, 1);
-	write(1, &d4, 1);
-	if (1)
-		write(1, ", ", 2);
-}
-
-void	ft_print_comb2(void)
+void	display(int a, int b)
 {
 	char d1;
 	char d2;
 	char d3;
 	char d4;
 
-	d1 = 47;
-	d2 = 47;
-	d3 = 48;
-	d4 = 48;
-	while (d1++ < 57)
+	d1 = a / 10 + '0';
+	d3 = b / 10 + '0';
+	d2 = a % 10 + '0';
+	d4 = b % 10 + '0';
+	write(1, &d1, 1);
+	write(1, &d2, 1);
+	write(1, " ", 1);
+	write(1, &d3, 1);
+	write(1, &d4, 1);
+	if (!(a == 98 && b == 99))
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb2(void)
+{
+	int d1;
+	int d2;
+
+	d1 = -1;
+	while (d1++ < 98)
 	{
-		d2
-		while (d2++ < '9')
+		d2 = d1;
+		while (d2++ < 99)
 		{
-			display(d1, d2, d3, d4);
-			d2++;
+			display(d1, d2);
 		}
-		d1++;
 	}
 }
--- main.c
+++ main.c
@@ -1,9 +1,6 @@
-//void	ft_print_comb2(void);
-#include <unistd.h>
+void ft_print_comb2(void);
+
 int main()
 {
-//	ft_print_comb2();
-	char c = '1';
-	c = '1'+ 1;
-	write(1, &c, 1);
+	ft_print_comb2();
 }
